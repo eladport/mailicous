45f2fdfc8a87e495c6a5d92d1dc3d4125bb11e1e
elad port <eladpor10@gmail.com>
2024-07-20 23:31:01 +0300

    python files

--- mail_from_client.py
+++ mail_from_client.py
@@ -7,6 +7,12 @@ smtp_server = 'localhost'
 smtp_port = 25  # Change to 25 if you're using the standard SMTP port
 sender_email = 'sender_test@example.com'
 receiver_email = 'recipient_test@example.com'
+dkim_signature = """v=1; a=rsa-sha256; c=relaxed/simple; d=example.com; s=default;
+  t=1620244561; bh=47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=;
+  h=From:To:Subject:Date:Message-ID;
+  b=Ib2g1ozUSUyXIn4K5JZQkdxIgzH2nFP/E2kzOcItSLi9pWQXOHozNco76E/xyfiN1
+   EN+6CI7gDfprN1FsOmkaerEqC/UHfPqIfa4JIRp8HU0w0sQ8j0BD8A/um8BsMGnElH
+   p8WzVPOWvnt6L7E4u5O38A08Q/AlrNlXit+bVDeT48s="""
 subject_benign = 'Benign Email'
 body_benign = """
                 Hello my dear company,
@@ -17,6 +23,8 @@ body_benign = """
                     Steve,
                     HR department
        """
+spf_result = 'pass (example.com: domain of sender@example.com designates 192.0.2.1 as permitted sender)'
+
 subject_malicious = 'malicious mail'
 SAFE_PLACEHOLDER = "<<safe_placeholder>>"
 # splitted with safe placeholder to avoid clicking
@@ -36,12 +44,20 @@ msg_benign['From'] = sender_email
 msg_benign['To'] = receiver_email
 msg_benign['Subject'] = subject_benign
 msg_benign.attach(MIMEText(body_benign, 'plain'))
+msg_benign.add_header('X-ASN', 'AS12345')
+msg_benign.add_header('X-AS-NAME', 'example ASN')
+msg_benign.add_header('Received-SPF', f'{spf_result} receiver={receiver_email}; client-ip=192.0.2.1; envelope-from={sender_email}; helo=mail.sender.com;')
+msg_benign['DKIM-Signature'] = dkim_signature
 
 msg_malicious = MIMEMultipart()
 msg_malicious['From'] = sender_email
 msg_malicious['To'] = receiver_email
 msg_malicious['Subject'] = subject_malicious
 msg_malicious.attach(MIMEText(MALICIOUS_EMAIL_CONTENT, 'plain'))
+msg_malicious.add_header('X-ASN', 'AS12345')
+msg_malicious.add_header('X-AS-NAME', 'example ASN')
+msg_malicious.add_header('Received-SPF', f'{spf_result} receiver={receiver_email}; client-ip=192.0.2.1; envelope-from={sender_email}; helo=mail.sender.com;')
+msg_malicious['DKIM-Signature'] = dkim_signature
 
 try:
     # Connect to the SMTP server
